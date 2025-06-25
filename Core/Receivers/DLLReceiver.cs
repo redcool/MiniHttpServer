9658e17738927921653894519439e4b92a168988
gamebox1 <redcool007@qq.com>
2025-06-25 15:07:15 +0800

    v0.0.1.12 update DLLReceiver, use Load

--- Core/Receivers/DLLReceiver.cs
+++ Core/Receivers/DLLReceiver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,7 +14,6 @@ namespace PowerUtilities.Net
     /// </summary>
     public class DLLReceiver
     {
-
         [RuntimeInitializeOnLoadMethod]
         static void Init()
         {
@@ -23,31 +23,31 @@ namespace PowerUtilities.Net
 
         private static void OnReceived(string fileName, string fileType, string filePath, List<MiniHttpKeyValuePair> headers)
         {
-            if(fileType.ToLower() == ".dll")
+            if (fileType.ToLower() != ".dll" || Application.platform == RuntimePlatform.IPhonePlayer)
             {
-                if(Application.platform == RuntimePlatform.IPhonePlayer)
-                {
-                    Debug.LogError("IPhone do not support JIT");
-                    return;
-                }
-
-                var asm = Assembly.LoadFile(filePath);
-                if(asm == null)
-                {
-                    Debug.Log($"[DLLReceiver], dll not found,{filePath}");
-                    return;
-                }
-
-                var defTypes = asm.DefinedTypes;
-                foreach (var defType in defTypes)
-                {
-                    //defType.InvokeMethod("Main", null, null, null);
-                    var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
-                    defType.GetMethod("Main", flags)?.Invoke(null, null);
-                }
-
+                return;
             }
 
+            var asmBytes = File.ReadAllBytes(filePath);
+            var asm = Assembly.Load(asmBytes);
+            CallAllMain(asm);
+        }
+
+         static void CallAllMain(Assembly asm)
+        {
+            if (asm == null)
+            {
+                Debug.Log($"[DLLReceiver], dll not found,{asm.FullName}");
+                return;
+            }
+
+            var defTypes = asm.DefinedTypes;
+            foreach (var defType in defTypes)
+            {
+                //defType.InvokeMethod("Main", null, null, null);
+                var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+                defType.GetMethod("Main", flags)?.Invoke(null, null);
+            }
         }
     }
 }
